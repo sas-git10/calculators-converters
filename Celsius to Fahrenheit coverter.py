1103c0af6095b2fb87aaf29c9f3e45470434f247
sas-git10 <siddharth.01010@gmail.com>
2026-08-14 13:30:37 +0530

    Add files via upload
    This is short script to convert temperature from Celsius to Fahrenheit

---
+++ Celsius to Fahrenheit coverter.py
@@ -0,0 +1,4 @@
+Celsius = int(input("Enter temperature in Celsius "))
+Fahrenheit = ((Celsius*1.8) + 32)
+print (Celsius, "degrees Celsius is", Fahrenheit, "degrees Fahrenheit")
+print ("Thank you for using this temperature converter")
